30a4751a5085b32ec97629ba3d28c7ddaf5d4d18
ace <94176142+optimisticace2003@users.noreply.github.com>
2024-07-26 14:08:31 +0530

    Add files via upload

---
+++ Display-website/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>bootstrap paradox</title>
+		<link rel="stylesheet" type="text/css" href="css/stylesheet1.css" media="screen"/>
+        <link rel="stylesheet" type="text/css" href="css/animate.css" />
+		<link rel="shortcut icon" href="images/favcon.ico" type="image/x-icon"/>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Rubik" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="keywords" content="Hackathon, Major League Hacking, Local Hack Day, Ridge Hacks, Iroquois Ridge High School"/>
+		<meta name="description" content="HackIR is an annual hackathon hosted by Iroquois Ridge High School."/>
+		<meta name="author" content="Jason Liu, Jianyuan Su, Victor Zhang">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	</head>
+
+<body style="overflow-x: hidden; overflow-y: auto">
+
+
+	<!--- Top Menu --->
+		<div class="nav1">
+            <div class="logo"><a href="#header"><img height="50" width="50" alt="college" src="images/Logo.jpg" ></a></div>
+			<div class="name"><a href="#header">Bootstrap Paradox</a></div>
+
+            <div class="container" style="height: auto">
+                <div class="bar1"></div>
+                <p class="bar2">menu</p>
+                <div class="bar3"></div>
+            </div>
+
+            <div class="dropdown hidden">
+                <div class="dd-content" id="myDropdown">
+                    <ul>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#location">Location</a></li>
+                        <li><a href="#schedule">Schedule</a></li>
+                        <li><a href="#faq">FAQ</a></li>
+                        <li><a href="#sponsors">Sponsors</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+	<!-- Header -->
+	<div id="header">
+        <div class="menu">
+			<div class="logo"><a href="#header"><img height="60" width="60" alt="Saveetha Logo" src="images/Logo.png" ></a></div>
+			<div class="name"><a href="#header">SAVEETHA</a></div>
+           	<ul>
+				<li><a href="#about" class>About</a></li>
+				<li><a href="#schedule" class>Instructions</a></li>
+				<li><a href="#faclty" class>Faculty</a></li>
+				<li><a href="#Contact" class>Contact</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div id="banner">
+	<img class="bannerimg"  src="images/background.png">
+		<div class="twobuttons">
+		<div id="about" class="registerposition"><a href="https://forms.gle/4q2FaCdQd9Mg1bUj7" target="_blank" class="register">Register!</a></div>
+		</div>
+	</div>
+	
+	<div class="content">
+		<div>
+			<h1>About</h1>
+			<hr class="ruler"/>
+			<p class="about">
+				Join us for an exciting coding competition... Get ready to embark on an extraordinary journey of innovation, creativity, and collaboration. We're thrilled to have you join us for this adrenaline-fueled event that celebrates the cutting-edge world of technology.  Whether you're a coding prodigy, a design virtuoso, or a problem-solving guru, this is your chance to shine. Over the next few days, you'll have the opportunity to tackle real-world challenges, build incredible projects, and network with fellow tech enthusiasts who share your passion. </p>
+			<p class="about"> As we gather under the banner of innovation, let's push the boundaries of what's possible. From mind-bending algorithms to elegant user interfaces, from artificial intelligence to blockchain wizardry – there's no limit to what you can achieve. Let the code compile, the circuits connect, and the creativity flow. Make new friends, learn from mentors, and seize the chance to make your mark on the tech landscape. So, gear up, put your thinking caps on, and let's code, hack, and create the future together! Welcome to the Bootstrap Paradox  2023 – where innovation knows no bounds. Happy hacking! </p>
+		</div>
+		
+		<div class="slideshow-container">
+			<div class="mySlides fade">
+  				<div class="numbertext">1 / 7</div>
+  				<img class="imgcrop" alt="Image 1" src="images/25.jpg" style="width:100%;">
+  				<div class="text"></div>
+				</div>
+
+			<div class="mySlides fade">
+  				<div class="numbertext">2 / 7</div>
+  				<img class="imgcrop" alt="Image 2" src="images/32.jpg" style="width:100%;">
+  				<div class="text"></div>
+			</div>
+
+			<div class="mySlides fade">
+  				<div class="numbertext">3 / 7</div>
+  				<img class="imgcrop" alt="Image 3" src="images/21.jpg" style="width:100%;" >
+  				<div class="text"></div>
+			</div>
+
+			<div class="mySlides fade">
+  				<div class="numbertext">4 / 7</div>
+  				<img class="imgcrop" alt="Image 4-13" src="images/13.jpg" style="width:100%;" >
+  				<div class="text"></div>
+			</div>
+	
+			<div class="mySlides fade">
+  				<div class="numbertext">5 / 7</div>
+  				<img class="imgcrop" alt="Image 5" src="images/38.jpg" style="width:100%;" >
+ 				<div class="text"></div>
+			</div>
+	
+			<div class="mySlides fade">
+  				<div class="numbertext">6 / 7</div>
+  				<img class="imgcrop" alt="Image 6" src="images/1.JPG" style="width:100%;" >
+  				<div class="text"></div>
+			</div>
+	
+			<div class="mySlides fade">
+  				<div class="numbertext">7 / 7</div>
+  				<img class="imgcrop" alt="Image 7" src="images/57.JPG" style="width:100%;" >
+  				<div class="text"></div>
+			</div>
+		</div>
+
+		<div class="threedots" style="text-align:center;">
+  			<span class="dot"></span> 
+  			<span class="dot"></span> 
+  			<span class="dot"></span> 
+ 	 		<span class="dot"></span> 
+ 	 		<span class="dot"></span> 
+ 	 		<span class="dot"></span> 
+  			<span class="dot"></span> 
+		</div>
+			<hr class="ruler"/>
+			<div class="mapouter">
+			</div>
+			<br>	
+		</div>
+	</div>
+		<br/>
+		<div id="schedule">
+			<h1>Instructions</h1><br>
+			<ul>
+				<li>30/9/2023: Deadline for Abstract Submission</li>
+				<li>Acceptance: within 12 hours from the date of submission</li>
+				<li>Last Date for Registration:4/10/2023</li>
+				<li>Registration Amount: Rs.500/- per person</li>
+				<li>Team Size: 1 to 4</li>
+			  </ul><br>
+		</div>
+		<hr>
+		<div id="faclty">
+			<h1>Faculty Coordinator</h1>
+			<p>1. Dr. P. Sriramya<br>
+				Professor/HOD, Dept. of Data Science<br>
+				</p>
+				<p>2. Dr. S.Magesh Kumar<br>
+				Professor/HOD, Dept. of Big Data & Network Security</p><br>
+		</div>
+		<hr>
+		<div id="Contact">
+			<h1>Contacts</H1><br>
+			<p>1. Mr. Hitesh Raj (7010802315)</p><br>
+			<p>2. Mr. Sreeram Ragavendra (9176407964)</p><br>
+			<p> 3. Mr. Bharathwaj (9840075725)</p><br>
+		</div>
+	<div class="footer">
+		<div class="socialmedia">
+			<a href="mailto:hackiroquoisridge@gmail.com" class="fa fa-email">&#9993;</a>
+			<a target="_blank" href="https://www.facebook.com/events/230825571061109/?active_tab=about" class="fa fa-facebook"></a>
+			<a target="_blank" href="https://www.instagram.com/irhshackathon/?hl=en" class="fa fa-instagram"></a>
+			<a target="_blank" href="https://www.linkedin.com/company/hackir/" class="fa fa-linkedin"></a>
+		</div>
+		<div style="padding: 2px;"></div>
+
+		<p style="margin: 8px; cursor: default">Bootstrap Paradox</p>
+	</div>
+	
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/temp.js"></script>
+	<script type="text/javascript" src="js/gallery.js"></script>
+	<!-- Scroll Down Feature -->
+	<script type="text/javascript" src="js/slideNav.js"></script>
+	<script>
+	    window.slide = new SlideNav();
+    </script>
+</body>
+</html>
